bd89e13d1abf3c6202d73c5a1879a7f6efcefbef
0adev <ayoubaguirar44@gmail.com>
2025-03-17 15:56:51 +0000

    Upload folders

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { Brand, Cta, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Header,
+  Footer,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+      </div>
+
+      <div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Possibility />
+        <Blog />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
